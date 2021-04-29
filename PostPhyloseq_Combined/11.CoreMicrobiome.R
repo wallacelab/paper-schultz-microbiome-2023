f59116ee1acb3759d300556cf35b581424d37e51
Corey Schultz <crs68219@uga.edu>
2021-04-29 16:47:09 -0400

    All the venn diagrams bby

--- PostPhyloseq_Combined/11.CoreMicrobiome.R
+++ PostPhyloseq_Combined/11.CoreMicrobiome.R
@@ -3,7 +3,7 @@
 
 library(tidyverse)
 library(devtools)
-devtools::install_github("jbisanz/qiime2R")
+#devtools::install_github("jbisanz/qiime2R")
 library(qiime2R)
 library(ggplot2)
 library(phyloseq)
@@ -126,12 +126,17 @@ rootsF
 
 stalksR <- microbiome::transform(stalksF, "compositional")
 rhizosR <- microbiome::transform(rhizosF, "compositional")
-
+phyCmbComp <- microbiome::transform(phyCmbFilt, "compositional")
+phy2com <- microbiome::transform(phy2, "compositional")
 ##############################################
-devtools::install_github("microbiome/microbiome")
+#devtools::install_github("microbiome/microbiome")
 library(microbiome)
 library(RColorBrewer)
 library(reshape)
+library(eulerr)
+#devtools::install_github('microsud/microbiomeutilities')
+library(microbiomeutilities)
+
 
 # test with stalks first
 stalksR # 660 taxa 79 samples - these are below 50% so ignore it
@@ -147,11 +152,14 @@ head(prevalence(rhizosR, detection = 1/100, sort = TRUE, count = TRUE)) # How ma
 prevalences <- seq(.05, 1, .05)
 detections <- 10^seq(log10(1e-3), log10(.2), length = 10)
 
+prevalences <- seq(.5, 1, .1)
+detections <- 10^seq(log10(1e-3), log10(.2), length = 10)
+
 # Also define gray color palette
 gray <- gray(seq(0,1,length=5))
 
 plot_core(rhizosR, plot.type = "heatmap", colours = gray,
-          prevalences = prevalences, detections = detections) +
+          prevalences = prevalences, detections = detections, min.prevalence = .5) +
   labs(x = "Detection Threshold (Relative Abundance (%))")
 
 #roots
@@ -160,10 +168,182 @@ head(prevalence(rootsF, detection = 1/100, sort = TRUE)) # Most Abundant OTUs an
 head(prevalence(rootsF, detection = 1/100, sort = TRUE, count = TRUE)) # How many samples are they actually in 
 
 
+#### Venn Diagram
+library("ggVennDiagram")
+#https://microbiome.github.io/tutorials/core_venn.html
+# how many samples for metadata groups
+
+### Experiment
+# For ultra filtered PhyCmbFilt - only taxa that they all share
+table(meta(phyCmbComp)$Experiment)
+ExperimentGroups <- unique(as.character(meta(phyCmbComp)$Experiment))
+print(ExperimentGroups)
+list_core <- c() # empty object
+for (n in ExperimentGroups){
+  print(as.name(n))
+  core.sub <- subset_samples(phyCmbComp, Experiment == n)
+  
+  core_m <- core_members(core.sub,                   # core.sub is only samples in experiment
+                         detection = .001,            # .001 in atleast 90% of samples
+                         prevalence = 0.5)          # 
+  print(paste0("No. of core taxa in ", n, " : ", length(core_m)))
+  list_core[[n]] <- core_m
+}
+
+GH_core_tax = as.data.frame(tax_table(phyCmbComp)[list_core$GH, ], row.names = NULL)
+END_core_taxa = as.data.frame(tax_table(phyCmbComp)[list_core$END, ], row.names = NULL)
+MMH_core_taxa = as.data.frame(tax_table(phyCmbComp)[list_core$MMH, ], row.names = NULL)
+
+ggVennDiagram(list_core) + scale_fill_gradient(low = "white", high = "firebrick") + ggtitle("Ultra Filtered Core Microbiome")
+
+# How to identify the actual taxa!!!!!
+itercept1 = merge(GH_core_tax, MMH_core_taxa, by="row.names")
+merge(GH_core_tax, END_core_taxa, by="row.names")
 
 
+# For less filtered phy2com - all taxa
+table(meta(phy2com)$Experiment)
+ExperimentGroups <- unique(as.character(meta(phy2com)$Experiment))
+print(ExperimentGroups)
+list_core <- c() # empty object
+for (n in ExperimentGroups){
+  print(as.name(n))
+  core.sub <- subset_samples(phy2com, Experiment == n)
+  
+  core_m <- core_members(core.sub,                   # core.sub is only samples in experiment
+                         detection = .0001,            # had to decrease stringency because there were so many taxa
+                         prevalence = 0.5)          # 
+  print(paste0("No. of core taxa in ", n, " : ", length(core_m)))
+  list_core[[n]] <- core_m
+}
+# Get taxa info
+GH_core_tax = as.data.frame(tax_table(phy2com)[list_core$GH, ], row.names = NULL)
+
+END_core_taxa = as.data.frame(tax_table(phy2com)[list_core$END, ], row.names = NULL)
+
+MMH_core_taxa = as.data.frame(tax_table(phy2com)[list_core$MMH, ], row.names = NULL)
+
+ggVennDiagram(list_core) + scale_fill_gradient(low = "white", high = "forestgreen") + ggtitle("All Taxa Core Microbiome")
+
+itercept1 = merge(GH_core_tax, MMH_core_taxa, by="row.names")
 
 
+### By Inbred/Hybrid
+# For ultra filtered PhyCmbFilt - only taxa that they all share
+table(meta(phyCmbComp)$Inbred_or_Hybrid)
+#BackgroundGroups <- unique(as.character(meta(phyCmbComp)$Inbred_or_Hybrid))
+BackgroundGroups <- c("Inbred", "Open_Pollinated", "Hybrid")      # don't want soil or blanks
+print(BackgroundGroups)
+list_core <- c() # empty object
+for (n in BackgroundGroups){
+  print(as.name(n))
+  core.sub <- subset_samples(phyCmbComp, BackgroundGroups == n)
+  
+  core_m <- core_members(core.sub,                   # core.sub is only samples in experiment
+                         detection = .001,            # .001 in atleast 90% of samples
+                         prevalence = 0.5)          # 
+  print(paste0("No. of core taxa in ", n, " : ", length(core_m)))
+  list_core[[n]] <- core_m
+}
+#Inbred_core_tax = data.frame()
+
+# Not sure what this issue is but had to change to this format and it takes a while
+Inbred_core_tax = as.data.frame(phyCmbComp@tax_table)[list_core$Inbred, ]
+Open_Pollinated_core_tax = as.data.frame(phyCmbComp@tax_table)[list_core$Open_Pollinated, ]
+Hybrid_core_tax = as.data.frame(phyCmbComp@tax_table)[list_core$Hybrid, ]
+
+ggVennDiagram(list_core) + scale_fill_gradient(low = "white", high = "firebrick") + ggtitle("Ultra Filtered Core Microbiome")
+
+# How to identify the actual taxa!!!!!
+itercept1 = merge(GH_core_tax, MMH_core_taxa, by="row.names")
+merge(GH_core_tax, END_core_taxa, by="row.names")
+
+
+# # For less filtered phy2com - all taxa
+table(meta(phy2com)$Inbred_or_Hybrid)
+#BackgroundGroups <- unique(as.character(meta(phyCmbComp)$Inbred_or_Hybrid))
+BackgroundGroups <- c("Inbred", "Open_Pollinated", "Hybrid")      # don't want soil or blanks
+print(BackgroundGroups)
+list_core <- c() # empty object
+for (n in BackgroundGroups){
+  print(as.name(n))
+  core.sub <- subset_samples(phy2com, BackgroundGroups == n)
+  
+  core_m <- core_members(core.sub,                   # core.sub is only samples in experiment
+                         detection = .0001,            # .001 in atleast 90% of samples
+                         prevalence = 0.5)          # 
+  print(paste0("No. of core taxa in ", n, " : ", length(core_m)))
+  list_core[[n]] <- core_m
+}
+#Inbred_core_tax = data.frame()
+
+# Not sure what this issue is
+Inbred_core_tax = as.data.frame(phy2com@tax_table)[list_core$Inbred, ]
+Open_Pollinated_core_tax = as.data.frame(phy2com@tax_table)[list_core$Open_Pollinated, ]
+Hybrid_core_tax = as.data.frame(phy2com@tax_table)[list_core$Hybrid, ]
+
+ggVennDiagram(list_core) + scale_fill_gradient(low = "white", high = "forestgreen") + ggtitle("All Taxa Core Microbiome")
+
+# How to identify the actual taxa!!!!!
+itercept1 = merge(GH_core_tax, MMH_core_taxa, by="row.names")
+merge(GH_core_tax, END_core_taxa, by="row.names")
+
+
+### By Sample Type
+# For ultra filtered PhyCmbFilt - only taxa that they all share
+table(meta(phyCmbComp)$Sample_Type)
+#BackgroundGroups <- unique(as.character(meta(phyCmbComp)$Inbred_or_Hybrid))
+SampleGroups <- c("Rhizosphere", "Root", "Stalk")      # don't want soil or blanks
+print(SampleGroups)
+list_core <- c() # empty object
+for (n in SampleGroups){
+  print(as.name(n))
+  core.sub <- subset_samples(phyCmbComp, SampleGroups == n)
+  
+  core_m <- core_members(core.sub,                   # core.sub is only samples in experiment
+                         detection = .001,            # .001 in atleast 90% of samples
+                         prevalence = 0.5)          # 
+  print(paste0("No. of core taxa in ", n, " : ", length(core_m)))
+  list_core[[n]] <- core_m
+}
+#Inbred_core_tax = data.frame()
+
+# Not sure what this issue is
+Rhizo_core_tax = as.data.frame(phyCmbComp@tax_table)[list_core$Rhizosphere, ]
+Roots_Pollinated_core_tax = as.data.frame(phyCmbComp@tax_table)[list_core$Root, ]
+Stalks_core_tax = as.data.frame(phyCmbComp@tax_table)[list_core$Stalk, ]
+
+ggVennDiagram(list_core) + scale_fill_gradient(low = "white", high = "firebrick") + ggtitle("Ultra Filtered Core Microbiome")
+
+# How to identify the actual taxa!!!!!
+itercept1 = merge(GH_core_tax, MMH_core_taxa, by="row.names")
+merge(Stalks_core_tax, Rhizo_core_tax, by="row.names")
+
+
+# # For less filtered phy2com - all taxa
+table(meta(phy2com)$Sample_Type)
+#BackgroundGroups <- unique(as.character(meta(phyCmbComp)$Inbred_or_Hybrid))
+SampleGroups <- c("Rhizosphere", "Root", "Stalk")      # don't want soil or blanks
+print(SampleGroups)
+list_core <- c() # empty object
+for (n in SampleGroups){
+  print(as.name(n))
+  core.sub <- subset_samples(phy2com, SampleGroups == n)
+  
+  core_m <- core_members(core.sub,                   # core.sub is only samples in experiment
+                         detection = .0001,            # .001 in atleast 90% of samples
+                         prevalence = 0.5)          # 
+  print(paste0("No. of core taxa in ", n, " : ", length(core_m)))
+  list_core[[n]] <- core_m
+}
+#Inbred_core_tax = data.frame()
+
+# Not sure what this issue is
+Rhizo_core_tax = as.data.frame(phy2com@tax_table)[list_core$Rhizosphere, ]
+Roots_Pollinated_core_tax = as.data.frame(phy2com@tax_table)[list_core$Root, ]
+Stalks_core_tax = as.data.frame(phy2com@tax_table)[list_core$Stalk, ]
+
+ggVennDiagram(list_core) + scale_fill_gradient(low = "white", high = "forestgreen") + ggtitle("All Taxa Core Microbiome")
 
 
 
